6b9ba959851b8b1a73f00ea29fccbaed5d9100ac
Stefan <Lapki.web@gmail.com>
2023-04-06 19:18:11 +0800

    add header + section screen + adaptive

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+  $('.menu__btn').on('click', (function () {
+    if (!$('.menu').hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.menu__btn').addClass('active');
+    } else {
+      $('.menu').removeClass('active');
+      $('.menu__btn').removeClass('active');
+    }
+  }));
+
+});
