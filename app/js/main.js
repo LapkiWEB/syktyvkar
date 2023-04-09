3f61f1859874b8386db09b4f007ea68a4a36f324
Stefan <Lapki.web@gmail.com>
2023-04-09 15:09:25 +0800

    add adaptive section services

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
+window.onscroll = function showHeader() {
+  let header = document.querySelector('.header'),
+    screen = document.querySelector('.screen');
+  if (window.pageYOffset > 300) {
+    header.classList.add('header__fixed');
+    screen.classList.add('screen-active');
+  } else {
+    header.classList.remove('header__fixed');
+    screen.classList.remove('screen-active');
+  }
+}
+
+
 $(function () {
 
   $('.menu__btn').on('click', (function () {
